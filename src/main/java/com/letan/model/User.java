64f619bd32b1f4481961f6ab478e3264cd0938ba
Tân <leetaan1902@gmail.com>
2023-09-25 22:47:25 +0700

    end

--- src/main/java/com/letan/model/User.java
+++ src/main/java/com/letan/model/User.java
@@ -3,16 +3,35 @@ package com.letan.model;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    private String lastName;
     private String firstName;
+    private String lastName;
     private String email;
+    private String hearAboutUs;
+    private String receiveAnnouncements;
+    private String contact;
 
-    public User() {}
+    public User() {
+        firstName = "";
+        lastName = "";
+        email = "";
+        hearAboutUs="";
+        receiveAnnouncements = "";
+    }
 
-    public User(String lastName, String firstName, String email) {
-        this.lastName = lastName;
+    public User(String firstName, String lastName, String email,String hearAboutUs,String receiveAnnouncements,String contact) {
         this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
+        this.hearAboutUs=hearAboutUs;
+        this.receiveAnnouncements = receiveAnnouncements;
+        this.contact = contact;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
@@ -23,19 +42,35 @@ public class User implements Serializable {
         this.lastName = lastName;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getHearAboutUs() {
+        return hearAboutUs;
+    }
+
+    public void setHearAboutUs(String hearAboutUs) {
+        this.hearAboutUs = hearAboutUs;
+    }
+
+    public String getReceiveAnnouncements() {
+        return receiveAnnouncements;
+    }
+
+    public void settReceiveAnnouncements(String receiveAnnouncements) {
+        this.receiveAnnouncements = receiveAnnouncements;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
     }
 }
--- src/main/java/com/letan/servlet/SurveyServlet.java
+++ src/main/java/com/letan/servlet/SurveyServlet.java
@@ -1,6 +1,4 @@
 package com.letan.servlet;
-
-import com.letan.form.SurveyForm;
 import com.letan.model.User;
 
 import java.io.IOException;
@@ -13,40 +11,59 @@ import javax.servlet.http.HttpServletResponse;
 public class SurveyServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-        request.setCharacterEncoding("UTF-8");
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
 
         String url = "/index.html";
 
+        // get current action
         String action = request.getParameter("action");
-
-        if (action == null)
-            action="join";
-
+        if (action == null) {
+            action = "join";  // default action
+        }
+        // perform action and set URL to appropriate page
         if (action.equals("join")) {
-            url = "/index.html";
-        } else if (action.equals("add")){
-            String lastName = request.getParameter("lastName");
+            url = "/index.html";    // the "join" page
+        }
+        else if (action.equals("add")) {
+            String receiveAnnouncements ="";
+            String hearAboutUs = request.getParameter("HearAboutUs");
+
+            // get parameters from the request
             String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
             String email = request.getParameter("email");
-            String wayPeopleKnow = request.getParameter("wayPeopleKnow");
-            String isReceiveNotification = "on".equals(request.getParameter("isReceiveNotification")) ? "YES" : "NO";
-            String contactType = request.getParameter("contactType");
-            User user = new User(lastName, firstName, email);
 
-            SurveyForm surveyForm = new SurveyForm(user, wayPeopleKnow, isReceiveNotification, contactType);
+            String _receiveAnnouncements = request.getParameter("yesILikeThat");
+            String contact = request.getParameter("contactMethod");
 
-            request.setAttribute("surveyForm", surveyForm);
-            url = "/thanks.jsp";
+            if(_receiveAnnouncements!=null)
+            {
+                receiveAnnouncements = "Yes";
+            }
+            else
+            {
+                receiveAnnouncements = "No";
+            }
+
+            // store data in User object and save User object in db
+            User user = new User(firstName, lastName, email,hearAboutUs,receiveAnnouncements,contact);
+
+            // set User object in request object and set URL
+            request.setAttribute("user", user);
+            url = "/thanks.jsp";   // the "thanks" page
         }
 
-        getServletContext().getRequestDispatcher(url).forward(request, response);
+        // forward request and response objects to specified URL
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
     }
-
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
         doPost(request, response);
     }
 }
